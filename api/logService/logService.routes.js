8636dd8f1f52d0bbb827b7a5d7df65501f02e652
nicole elezra <im.nicole.m@gmail.com>
2022-02-23 12:04:37 +0200

    removing unnecessary logs

--- api/logService/logService.routes.js
+++ api/logService/logService.routes.js
@@ -1,21 +1,8 @@
 const express = require('express');
-const { requireAuth } = require('../../middlewares/requireAuth.middleware');
-const { log } = require('../../middlewares/logger.middleware');
-const {
-  // getlogServices,
-  // getlogService,
-  addlogService,
-  // updatelogService,
-  // removelogService,
-} = require('./logService.controller');
+
+const { addlogService } = require('./logService.controller');
 const router = express.Router();
 
 router.post('/', addlogService);
-// router.get('/', log, getlogServices);
-// router.get('/:id', getlogService);
-// router.put('/:toyId',requireAuth, updatelogService)
-// router.delete('/:toyId',requireAuth, removelogService)
-// router.put('/:id', updatelogService);
-// router.delete('/:id', removelogService);
 
 module.exports = router;
